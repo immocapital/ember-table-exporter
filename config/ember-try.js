ad70375e145a45967b4173d472b50354c6e54a24
Nino Annighoefer <nino@ninoan.com>
2019-04-23 15:41:28 +0100

    :cherry_blossom: Fix JS codestyle

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,82 +1,82 @@
-"use strict";
+'use strict';
 
-const getChannelURL = require("ember-source-channel-url");
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function() {
   return Promise.all([
-    getChannelURL("release"),
-    getChannelURL("beta"),
-    getChannelURL("canary")
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary'),
   ]).then(urls => {
     return {
-      useYarn: true,
-      scenarios: [
+      'useYarn': true,
+      'scenarios': [
         {
-          name: "ember-lts-2.18",
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              "jquery-integration": true
-            })
+          'name': 'ember-lts-2.18',
+          'env': {
+            'EMBER_OPTIONAL_FEATURES': JSON.stringify({
+              'jquery-integration': true,
+            }),
           },
-          npm: {
-            devDependencies: {
-              "@ember/jquery": "^0.5.1",
-              "ember-source": "~2.18.0"
-            }
-          }
-        },
-        {
-          name: "ember-lts-3.4",
-          npm: {
-            devDependencies: {
-              "ember-source": "~3.4.0"
-            }
-          }
-        },
-        {
-          name: "ember-release",
-          npm: {
-            devDependencies: {
-              "ember-source": urls[0]
-            }
-          }
-        },
-        {
-          name: "ember-beta",
-          npm: {
-            devDependencies: {
-              "ember-source": urls[1]
-            }
-          }
-        },
-        {
-          name: "ember-canary",
-          npm: {
-            devDependencies: {
-              "ember-source": urls[2]
-            }
-          }
-        },
-        {
-          name: "ember-default",
-          npm: {
-            devDependencies: {}
-          }
-        },
-        {
-          name: "ember-default-with-jquery",
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              "jquery-integration": true
-            })
+          'npm': {
+            'devDependencies': {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.18.0',
+            },
           },
-          npm: {
-            devDependencies: {
-              "@ember/jquery": "^0.5.1"
-            }
-          }
-        }
-      ]
+        },
+        {
+          'name': 'ember-lts-3.4',
+          'npm': {
+            'devDependencies': {
+              'ember-source': '~3.4.0',
+            },
+          },
+        },
+        {
+          'name': 'ember-release',
+          'npm': {
+            'devDependencies': {
+              'ember-source': urls[0],
+            },
+          },
+        },
+        {
+          'name': 'ember-beta',
+          'npm': {
+            'devDependencies': {
+              'ember-source': urls[1],
+            },
+          },
+        },
+        {
+          'name': 'ember-canary',
+          'npm': {
+            'devDependencies': {
+              'ember-source': urls[2],
+            },
+          },
+        },
+        {
+          'name': 'ember-default',
+          'npm': {
+            'devDependencies': {},
+          },
+        },
+        {
+          'name': 'ember-default-with-jquery',
+          'env': {
+            'EMBER_OPTIONAL_FEATURES': JSON.stringify({
+              'jquery-integration': true,
+            }),
+          },
+          'npm': {
+            'devDependencies': {
+              '@ember/jquery': '^0.5.1',
+            },
+          },
+        },
+      ],
     };
   });
 };
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
-import Application from "../app";
-import config from "../config/environment";
-import { setApplication } from "@ember/test-helpers";
-import start from "ember-exam/test-support/start";
+import Application from '../app';
+import config from '../config/environment';
+import { setApplication } from '@ember/test-helpers';
+import start from 'ember-exam/test-support/start';
 
 setApplication(Application.create(config.APP));
 
